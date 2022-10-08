6a56df903b2a0395fa8421f382431d1536ead8ae
Boston343 <branden.austin1@gmail.com>
2022-10-08 01:07:12 -0400

    this now does a post request with new user data to mailchimp server, to add it to my email list.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';          // npm install express
 import path from 'path';
 import https from 'https';              // for forming external get requests
 import { fileURLToPath } from 'url';
+import { mailchimpAPI } from './apikeys.js';    // Mailchimp API - used to manage newsletter distribution list
 const app = express();
 const port = 3000;
 app.use(express.urlencoded({extended: true}));  // this is for parsing data from html form
@@ -28,7 +29,7 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     console.log("Server is up and running.");
 
-    var fileName = "signup.html";
+    const fileName = "signup.html";
     res.sendFile(path.join(__dirname, "/" + fileName));
     console.log('Sent:', __dirname + "/" + fileName);
     
@@ -38,11 +39,47 @@ app.get('/', (req, res) => {
 // -------------------- Post Requests --------------------------
 // -------------------------------------------------------------
 app.post('/', (req, res) => {
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName; 
-    var email = req.body.email;  
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName; 
+    const email = req.body.email;  
 
     console.log("First Name: " + firstName);
     console.log("Last Name: " + lastName);
     console.log("Email: " + email);
+
+    // create mailchimp data JSON to batch subscribe or unsubscribe users
+    // https://mailchimp.com/developer/marketing/api/list-member-events/
+    const newData = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    // turn the JSON object into one long string for sending
+    const jsonData = JSON.stringify(newData);
+
+    // setup mailchimp post request 
+    const mailchimpURL = "https://" + mailchimpAPI.server + ".api.mailchimp.com/3.0/lists/" + mailchimpAPI.listid;
+    const options = {
+        method: "POST",
+        auth: "Reap3r:" + mailchimpAPI.key 
+    }
+
+    // create mailchimp post request
+    const mailchimpRequest = https.request(mailchimpURL, options, (response) => {
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    // make mailchimp post request with our data to add a person to email list
+    mailchimpRequest.write(jsonData);
+    mailchimpRequest.end();
 });
