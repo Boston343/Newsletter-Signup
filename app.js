dcb3d9bc37c93339679dd97ee5a23a504fbd9f3f
Boston343 <branden.austin1@gmail.com>
2022-10-09 18:34:13 -0400

    re-organized, and added process.env.PORT for deployment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import https from 'https';              // for forming external get requests
 import { fileURLToPath } from 'url';
 import { mailchimpAPI } from './apikeys.js';    // Mailchimp API - used to manage newsletter distribution list
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));  // this is for parsing data from html form
 
 // __dirname is only available with CJS. Since I am using ESM I need to get it another way
@@ -30,8 +30,8 @@ app.get('/', (req, res) => {
     console.log("Server is up and running.");
 
     const fileName = "signup.html";
-    res.sendFile(path.join(__dirname, "\\" + fileName));
-    console.log('Sent:', __dirname + "\\" + fileName);
+    res.sendFile(path.join(__dirname, "/src/" + fileName));
+    console.log('Sent:', __dirname + "/src/" + fileName);
     
 });
 
@@ -44,9 +44,9 @@ app.post('/', (req, res) => {
     const lastName = req.body.lastName; 
     const email = req.body.email;  
 
-    console.log("First Name: " + firstName);
-    console.log("Last Name: " + lastName);
-    console.log("Email: " + email);
+    // console.log("First Name: " + firstName);
+    // console.log("Last Name: " + lastName);
+    // console.log("Email: " + email);
 
     // create mailchimp data JSON to batch subscribe or unsubscribe users
     // https://mailchimp.com/developer/marketing/api/list-member-events/
@@ -101,8 +101,8 @@ app.post('/', (req, res) => {
                 htmlResponsePage = "failure.html";
             }
 
-            res.sendFile(path.join(__dirname, "\\" + htmlResponsePage));
-            console.log("Sent: " + __dirname + "\\" + htmlResponsePage);
+            res.sendFile(path.join(__dirname, "/src/" + htmlResponsePage));
+            console.log("Sent: " + __dirname + "/src/" + htmlResponsePage);
         });
     });
 
